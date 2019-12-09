f2686c74e5b9432e9737b6eca235a16b33b03fd6
Alpha <alcfa.onway@gmail.com>
2019-12-09 14:58:33 -0600

    add button

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,9 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+const button = document.querySelector('button');
+
+button.onclick = function() {
+  let name = prompt('What is your name?');
+  alert('Hello ' + name + ', nice to see you!');
+}
